177c20c95592dc23ffec9b03ff529aa0d9f1a8d7
Raphaela Rudnik <Rapharudnik@gmail.com>
2026-09-07 19:22:37 +0100

    organização de estrutura de classes em about.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,14 +5,12 @@ function About() {
     <section className="about">
       <h2 className="about__title">Sobre mim</h2>
       <p className="about__text">
-        <p className="about__text">
-          Oi, eu sou a Raphaela, mas pode me chamar de Rapha, sou formada em
-          estética e cosmética. Estou fazendo transição de carreira para a área
-          de desenvolvimento web e também sou criadora de conteúdo para internet
-          — resolvi juntar tudo, e assim nasceu o Glow Lab. O gosto pela
-          resolução de problemas me trouxe até a programação, e isso me motiva a
-          ser melhor pelo menos 1% todos os dias.
-        </p>
+        Oi, eu sou a Raphaela, mas pode me chamar de Rapha, sou formada em
+        estética e cosmética. Estou fazendo transição de carreira para a área de
+        desenvolvimento web e também sou criadora de conteúdo para internet —
+        resolvi juntar tudo, e assim nasceu o Glow Lab. O gosto pela resolução
+        de problemas me trouxe até a programação, e isso me motiva a ser melhor
+        pelo menos 1% todos os dias.
       </p>
     </section>
   );
